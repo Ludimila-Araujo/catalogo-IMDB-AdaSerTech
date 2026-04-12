7221eb3173be786062257bcdbe357fec2fe249aa
Enei Pereira Junior <eneipereira@outlook.com.br>
2026-04-12 00:21:56 -0300

    feat: Implementa classe FilmeService e cria método cadastrarFilme, que adiciona um novo filme à lista e imprime um texto de confirmação de cadastro com o nome do filme.

---
+++ src/service/FilmeService.java
@@ -0,0 +1,19 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Filme;
+
+public class FilmeService {
+
+    private final List<Filme> filmes;
+
+    public FilmeService() {
+        this.filmes = new ArrayList<>();
+    }
+
+    public void cadastrarFilme(Filme filme) {
+        filmes.add(filme);
+        System.out.println("Filme cadastrado com sucesso: " + filme.getNome());
+    }
+}
